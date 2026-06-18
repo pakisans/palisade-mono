1b2abfe79f29f31bf0101d01c03380bdc516ed64
pakisans <pakisakac@gmail.com>
2026-06-18 19:21:02 +0200

    migrate

--- backend/src/migrations/index.ts
+++ backend/src/migrations/index.ts
@@ -1,33 +1,9 @@
-import * as migration_20260325_222610 from './20260325_222610';
-import * as migration_20260325_223736 from './20260325_223736';
-import * as migration_20260329_221620 from './20260329_221620';
-import * as migration_20260406_062532 from './20260406_062532';
-import * as migration_20260520_113552 from './20260520_113552';
+import * as migration_20260618_172014_initial from './20260618_172014_initial';
 
 export const migrations = [
   {
-    up: migration_20260325_222610.up,
-    down: migration_20260325_222610.down,
-    name: '20260325_222610',
-  },
-  {
-    up: migration_20260325_223736.up,
-    down: migration_20260325_223736.down,
-    name: '20260325_223736',
-  },
-  {
-    up: migration_20260329_221620.up,
-    down: migration_20260329_221620.down,
-    name: '20260329_221620',
-  },
-  {
-    up: migration_20260406_062532.up,
-    down: migration_20260406_062532.down,
-    name: '20260406_062532',
-  },
-  {
-    up: migration_20260520_113552.up,
-    down: migration_20260520_113552.down,
-    name: '20260520_113552'
+    up: migration_20260618_172014_initial.up,
+    down: migration_20260618_172014_initial.down,
+    name: '20260618_172014_initial'
   },
 ];
